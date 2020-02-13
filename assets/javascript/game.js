143fa2a4ffd7347d49f6316b1dd6a87cc1678afc
Ragnorok303 <jasontompkins22@gmail.com>
2020-02-13 13:17:06 -0700

    Deleted some img. and Worked on debugging my code.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,66 +1,66 @@
-var words = ["leonardo", "raphael", "donatello", "michaelangelo", "splinter", "april oneal", "shredder", "pizza", "nyc", "footclan", "casey jones", "krang",]
+var words = ["leonardo", "raphael", "donatello", "michaelangelo", "splinter", "april oneal", "shredder", "pizza", "nyc", "footclan", "casey jones", "krang","ooze"]
 
 var computerChoice = "";
-var lettersOfWord = []
-var blanks = 0;
-var blanksAndCorrect = [];
+var letterChoice = []
+var empty = 0;
+var lettersCorrect = [];
 var wrongGuess = [];
 var wins = 0;
 var losses = 0;
-var guessesRemaining = 14;
+var numberRemain = 14;
 
 function Game() {
     computerChoice = words[Math.floor(Math.random() * words.length)];
-    lettersOfWord = computerChoice.split("");
-    blanks = lettersOfWord.length;
-    for (var i = 0; i < blanks; i++) {
-        blanksAndCorrect.push("_");
+    letterChoice = computerChoice.split("");
+    empty = letterChoice.length;
+    for (var i = 0; i < empty; i++) {
+        lettersCorrect.push("_");
     }
-    document.getElementById("currentword").innerHTML = "  " + blanksAndCorrect.join("  ");
+    document.getElementById("wordGuess").textContent = "  " + lettersCorrect.join("  ");
     console.log(computerChoice);
-    console.log(lettersOfWord)
-    console.log(blanks)
-    console.log(blanksAndCorrect)
+    console.log(letterChoice)
+    console.log(empty)
+    console.log(lettersCorrect)
 }
 function reset() {
-    guessesRemaining = 14;
+    numberRemain = 14;
     wrongGuess = [];
-    blanksAndCorrect = [];
+    lettersCorrect = [];
     Game()
 }
 function checkLetters(letter) {
     var letterInWord = false;
-    for (var i = 0; i < blanks; i++) {
+    for (var i = 0; i < empty; i++) {
         if (computerChoice[i] == letter) {
             letterInWord = true;
         }
     }
     if (letterInWord) {
-        for (var i = 0; i < blanks; i++) {
+        for (var i = 0; i < empty; i++) {
             if (computerChoice[i] == letter) {
-                blanksAndCorrect[i] = letter;
+                lettersCorrect[i] = letter;
             }
         }
     }
     wrongGuess.push(letter);
-    guessesRemaining--;
-    console.log(blanksAndCorrect);
+    numberRemain--;
+    console.log(lettersCorrect);
 }
 
 function complete() {
-    console.log("wins:" + wins + "| losses:" + losses + "| guesses left:" + guessesRemaining)
-    if (lettersOfWord.toString() == blanksAndCorrect.toString()) {
+    console.log("wins:" + wins + "| losses:" + losses + "| guesses left:" + numberRemain)
+    if (letterChoice.toString() == lettersCorrect.toString()) {
         wins++;
         reset()
-        document.getElementById("winstracker").innerHTML = " " + wins;
-    } else if (guessesRemaining === 0) {
+        document.getElementById("winstracker").textContent = " " + wins;
+    } else if (numberRemain === 0) {
         losses++;
         reset()
         document.getElementById("image").src = "./assets/images/Foot-Clan.jpg"
-        document.getElementById("losstracker").innerHTML = " " + losses;
+        document.getElementById("losestracker").textContent = " " + losses;
     }
-    document.getElementById("currentword").innerHTML = "  " + blanksAndCorrect.join(" ");
-    document.getElementById("guessesremaining").innerHTML = " " + guessesRemaining;
+    document.getElementById("wordGuess").textContent = "  " + lettersCorrect.join(" ");
+    document.getElementById("numberRemain").textContent = " " + numberRemain;
 }
 Game()
 document.onkeyup = function (event) {
@@ -68,5 +68,5 @@ document.onkeyup = function (event) {
     checkLetters(guesses);
     complete();
     console.log(guesses);
-    document.getElementById("playerguesses").innerHTML = "  " + wrongGuess.join(" ");
+    document.getElementById("userguess").textContent = "  " + wrongGuess.join(" ");
 }
